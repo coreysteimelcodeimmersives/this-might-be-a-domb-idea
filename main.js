fb8f0c63d1a6d9a2fe0829bec3c1e44377efe9ea
Corey Steimel <coreysteimel@Coreys-MacBook-Pro-Work.local>
2022-03-09 14:15:50 -0700

    finsihed.

---
+++ main.js
@@ -0,0 +1,23 @@
+let paragraph = document.querySelector('p');
+paragraph.style.color = 'lightblue';
+paragraph.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+let heading = document.querySelector('h1');
+heading.style.fontSize = '48px';
+
+let item13 = document.querySelector('#item-13');
+item13.style.opacity = '0.5';
+
+let item3 = document.querySelector('#item-3');
+item3.innerText = 'say, "Hi!"';
+
+let img = document.querySelector('img');
+img.src = 'http://www.tioxic.com/wp-content/uploads/trex_4.jpg';
+img.style.height = '300px';
+
+let img2 = document.querySelector('#img-2');
+img2.style.height = '300px';
+
+let item16 = document.querySelector('#item-16');
+item16.className = item3;
+item16.innerText = "Won't get fooled again.";
